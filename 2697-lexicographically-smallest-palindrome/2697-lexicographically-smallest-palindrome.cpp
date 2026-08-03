d5abbfba3854b9e54c24cdfbfabf9022c6ddd87f
cvsg7771-web <cvsg7771@gmail.com>
2026-08-03 19:31:36 +0530

    Time: 3 ms (76.3%), Space: 18.4 MB (87.68%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int a = 0;
+        int b = s.length()-1;
+        while(a<b)
+        {
+            if(s[a]!=s[b])
+            {
+              if(s[a]<s[b])
+              {
+                s[b]=s[a];
+                
+              }
+              else
+              {
+                s[a]=s[b];
+                
+              }
+              
+
+
+            }
+            a++;
+              b--;
+        }
+        return s;
+        
+    }
+    };

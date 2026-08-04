3938231a5bbc331ea1c9449a5d43939c87669b2c
cvsg7771-web <cvsg7771@gmail.com>
2026-08-05 00:18:03 +0530

    Time: 3 ms (44.23%), Space: 8.4 MB (92.78%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int b=x;
+        if(x<0||(x%10==0&&x!=0))
+        {
+            return false;
+
+        }
+        int rev=0;
+        
+
+        while(x>rev)
+        {
+           rev=rev*10+x%10;
+           x/=10;
+            
+        }
+        return x==rev||rev/10==x;
+
+        
+    }
+};

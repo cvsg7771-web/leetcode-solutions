c1699eed02920f4d88eedd8867a231d3746f8bea
cvsg7771-web <cvsg7771@gmail.com>
2026-09-04 10:33:21 +0530

    Time: 0 ms (100%), Space: 32.5 MB (5.99%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>pg(n);
+        vector<int>ns(n);
+        stack<int> st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty()&&st.top()<=nums[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                pg[i]=nums[i];
+                st.push(nums[i]);
+            }
+            else
+            {
+                pg[i]=st.top();
+            }
+        }
+        while(!st.empty())
+        {
+            st.pop();
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty()&&st.top()>=nums[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                ns[i]=nums[i];
+                st.push(nums[i]);
+            }
+            else
+            {
+                ns[i]=st.top();
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if((pg[i]-ns[i])<=k)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
